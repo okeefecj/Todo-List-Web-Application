caca2ad8d04055500706b56e8197c2407305d95e
Colin O'Keefe <okeefecj@lemoyne.edu>
2021-06-10 14:31:52 -0400

    Updated Error handling for welcome page

--- MavenProj/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ MavenProj/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,11 +1,13 @@
 package com.in28minutes.rest.webservices.restfulwebservices;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 //Controller
 @RestController
+@CrossOrigin(origins="http://localhost:4200")
 public class HelloWorldController {
 
 	//method returns hello world
@@ -21,13 +23,14 @@ public class HelloWorldController {
 	//hello-world-bean
 	@GetMapping(path="/hello-world-bean")
 	public HelloWorldBean HelloWorldBean() {
-		return new HelloWorldBean("Hello World");
+		return new HelloWorldBean("Hello World Bean");
 	}
 	
 	//hello-world/path-variable/in28minutes
 	@GetMapping(path="/hello-world/path-variable/{name}")
 	public HelloWorldBean HelloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World, %s", name));
+//		throw new RuntimeException("Something went wrong");
 		}
 		
 	
--- todo-app/src/api/todo/HelloWorldService.js
+++ todo-app/src/api/todo/HelloWorldService.js
@@ -4,6 +4,15 @@ class HelloWorldService{
     executeHelloWorldService() {
         return axios.get('http://localhost:8080/hello-world')
     }
+
+    executeHelloWorldBeanService() {
+        return axios.get('http://localhost:8080/hello-world-bean')
+    }
+
+    executeHelloWorldPathVariablService(name){
+        return axios.get(`http://localhost:8080/hello-world/path-variable/${name}`)
+    }
 }
+
 
 export default new HelloWorldService()
