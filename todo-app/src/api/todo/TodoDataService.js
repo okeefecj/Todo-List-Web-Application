f6864ae643b8ff06f48fa1b4b9a27c0d0f5b2c3e
Colin O'Keefe <okeefecj@lemoyne.edu>
2021-06-14 12:32:03 -0400

    Added delete function to front end

--- todo-app/src/api/todo/TodoDataService.js
+++ todo-app/src/api/todo/TodoDataService.js
@@ -7,6 +7,10 @@ class TodoDataService{
         return axios.get(`http://localhost:8080/users/${name}/todos`)
     }
 
+    deleteTodo(name, id){
+        return axios.delete(`http://localhost:8080/users/${name}/todos/${id}`)
+    }
+
 }
 
 export default new TodoDataService
--- todo-app/src/components/todo/ListToDoComponent.jsx
+++ todo-app/src/components/todo/ListToDoComponent.jsx
@@ -7,21 +7,24 @@ class ListToDoComponent extends Component {
     super(props);
     this.state = {
       todo: [],
+      message: null,
     };
+    this.deleteTodoClicked = this.deleteTodoClicked.bind(this);
   }
 
   componentDidMount() {
-    let username = AuthenticationService.getLoggedInName();
-    TodoDataService.retrieveAllTodos(username).then((response) => {
-      console.log(response);
-      this.setState({ todo: response.data });
-    });
+    console.log("componentDidMount");
+    this.refreshTodos();
+    console.log(this.state);
   }
 
   render() {
     return (
       <div>
         <h1>To Do List</h1>
+        {this.state.message && (
+          <div className="alert alert-success">{this.state.message}</div>
+        )}
         <div className="container">
           <table className="table">
             <thead>
@@ -29,6 +32,7 @@ class ListToDoComponent extends Component {
                 <th>Description</th>
                 <th>Finished</th>
                 <th>Target Date</th>
+                <th>Delete</th>
               </tr>
             </thead>
 
@@ -38,6 +42,14 @@ class ListToDoComponent extends Component {
                   <td>{todo.description}</td>
                   <td>{todo.isCompleted.toString()}</td>
                   <td>{todo.targetDate.toString()}</td>
+                  <td>
+                    <button
+                      className="btn btn-warning"
+                      onClick={() => this.deleteTodoClicked(todo.id)}
+                    >
+                      Delete
+                    </button>
+                  </td>
                 </tr>
               ))}
             </tbody>
@@ -46,6 +58,22 @@ class ListToDoComponent extends Component {
       </div>
     );
   }
+
+  deleteTodoClicked(id) {
+    let username = AuthenticationService.getLoggedInName();
+    TodoDataService.deleteTodo(username, id).then((repsonse) => {
+      this.setState({ message: `Delete of todo ${id} successful` });
+      this.refreshTodos();
+    });
+  }
+
+  refreshTodos = () => {
+    let username = AuthenticationService.getLoggedInName();
+    TodoDataService.retrieveAllTodos(username).then((response) => {
+      console.log(response);
+      this.setState({ todo: response.data });
+    });
+  };
 }
 
 export default ListToDoComponent;
